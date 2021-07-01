0c1424f6b111a2c2b7a49e060a7f316dd49a7359
serhiibulynin <serhiibulynin007@gmail.com>
2021-07-01 19:21:57 +0300

    поставил корректную дату

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ class CountdownTimer {
 
 const timer = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2020'),
+    targetDate: new Date('Jul 17, 2021'),
 })
 
 
